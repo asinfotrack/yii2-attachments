464d13066ecee53bea9184069389319adba97c82
Thomas Lutzenberger <thomas.lutzenberger@asinfotrack.ch>
2018-04-06 09:46:52 +0200

    Add callback handling avatar action

--- components/AvatarAction.php
+++ components/AvatarAction.php
@@ -13,33 +13,23 @@ class AvatarAction extends \yii\base\Action
 
 	/**
 	 * @var callable a callback to make final adjustments and modifications for the avatar before it gets
-	 * downloaded. Use the signature: `function ($img)` and return the image object itself.
-	 *
-	 * The `$img` param is an instance of `\yii\image\ImageDriver`.
-	 *
-	 * @see \yii\image\ImageDriver
+	 * downloaded. Use the signature: `function ($path)` and return the image data the action should return.
 	 */
 	public $imagePreparationCallback;
 
 	/**
 	 * The run action
 	 *
-	 * @param array $args the arguments passed to the action
+	 * @param mixed $id the arguments passed to the action
 	 * @return mixed the response data
 	 */
-	public function run($args)
+	public function run($id)
 	{
-		//check required params
-		if (!isset($args['id'])) {
-			$msg = Yii::t('app', 'Param `id` is missing');
-			throw new InvalidCallException($msg);
-		}
-
 		//fetch and validate model
 		$query = call_user_func([Module::getInstance()->classMap['attachmentModel'], 'find']);
-		$model = $query->where(['attachment.id'=>$args['id']])->isAvatar(true)->one();
+		$model = $query->where(['attachment.id'=>$id])->isAvatar(true)->one();
 		if ($model === null) {
-			$msg = Yii::t('app', 'No attachment with the id `{id}` found or it is not marked as an avatar', ['id'=>$args['id']]);
+			$msg = Yii::t('app', 'No attachment with the id `{id}` found or it is not marked as an avatar', ['id'=>$id]);
 			throw new NotFoundHttpException($msg);
 		}
 
@@ -49,9 +39,8 @@ class AvatarAction extends \yii\base\Action
 		$response->getHeaders()->set('Content-Type', $model->mime_type);
 
 		//prepare the image
-		if ($this->imagePreparationCallback !== null) {
-			//TODO: implement
-			throw new NotSupportedException('Not yet implemented!');
+		if (is_callable($this->imagePreparationCallback)) {
+			return call_user_func($this->imagePreparationCallback, $model->absolutePath);
 		} else {
 			$response->getHeaders()->set('Content-Length', $model->size);
 			return file_get_contents($model->absolutePath);
